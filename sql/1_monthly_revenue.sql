2276c48d964ed4c1f0a5c998ad1af51b5f5d0ebc
Maged Bin Laswad | Data Engineer <magedbinlaswad@gmail.com>
2026-02-24 02:54:49 +0300

    Update 1_monthly_revenue.sql

--- sql/1_monthly_revenue.sql
+++ sql/1_monthly_revenue.sql
@@ -1,1 +1,14 @@
+/*
+Monthly Revenue Analysis
+*/
 
+SELECT 
+    YEAR(Invoice_Date) AS Year,
+    MONTH(Invoice_Date) AS Month,
+    SUM(Revenue) AS Monthly_Revenue
+FROM retail_sales
+GROUP BY 
+    YEAR(Invoice_Date),
+    MONTH(Invoice_Date)
+ORDER BY 
+    Year, Month;
