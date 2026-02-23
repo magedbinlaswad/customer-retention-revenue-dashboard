75b8ec95ed24b7ffc58cee9941895707016fd118
Maged Bin Laswad | Data Engineer <magedbinlaswad@gmail.com>
2026-02-24 02:55:39 +0300

    Add SQL query for repeat customers
    Query to find customers with more than one order.

--- sql/3_repeat_customers.sql
+++ sql/3_repeat_customers.sql
@@ -1,1 +1,11 @@
+/*
+Repeat Customers (More than 1 Order)
+*/
 
+SELECT 
+    Customer_ID,
+    COUNT(DISTINCT Invoice_ID) AS Order_Count
+FROM retail_sales
+GROUP BY Customer_ID
+HAVING COUNT(DISTINCT Invoice_ID) > 1
+ORDER BY Order_Count DESC;
