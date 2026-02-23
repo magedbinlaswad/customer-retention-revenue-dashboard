2dfa607567edc814bacfd0a46651594856f9b092
Maged Bin Laswad | Data Engineer <magedbinlaswad@gmail.com>
2026-02-24 02:56:43 +0300

    Create SQL query for monthly order volume
    Added a SQL query to calculate monthly order volume.

--- sql/6_monthly_order_volume.sql
+++ sql/6_monthly_order_volume.sql
@@ -1,1 +1,13 @@
+/*
+Monthly Order Volume
+*/
 
+SELECT 
+    YEAR(Invoice_Date) AS Year,
+    MONTH(Invoice_Date) AS Month,
+    COUNT(DISTINCT Invoice_ID) AS Total_Orders
+FROM retail_sales
+GROUP BY 
+    YEAR(Invoice_Date),
+    MONTH(Invoice_Date)
+ORDER BY Year, Month;
