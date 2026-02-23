6b6afaf9da1cc1c84ecb86b646656001cc499946
Maged Bin Laswad | Data Engineer <magedbinlaswad@gmail.com>
2026-02-24 02:57:32 +0300

    Create query for high value customers using CTE
    Added a CTE to identify high value customers based on total spending.

--- sql/8_high_value_customers.sql
+++ sql/8_high_value_customers.sql
@@ -1,1 +1,16 @@
+/*
+High Value Customers using CTE
+*/
 
+WITH CustomerSpending AS (
+    SELECT 
+        Customer_ID,
+        SUM(Revenue) AS Total_Spent
+    FROM retail_sales
+    GROUP BY Customer_ID
+)
+
+SELECT *
+FROM CustomerSpending
+WHERE Total_Spent > 5000
+ORDER BY Total_Spent DESC;
